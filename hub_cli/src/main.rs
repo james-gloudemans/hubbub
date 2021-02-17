4f1445f852e050d9f6b614a480396230f84531d8
James Gloudemans <james.gloudemans@gmail.com>
2021-02-17 16:08:49 -0500

    Minimal CLI definition and matching

--- hub_cli/src/main.rs
+++ hub_cli/src/main.rs
@@ -1,3 +1,45 @@
+#![allow(unused_variables)]
+
+extern crate clap;
+extern crate hubbublib;
+use clap::{App, AppSettings, SubCommand};
+use hubbublib::hub::Hub;
+use hubbublib::msg::Message;
+use hubbublib::HubEntity;
+
 fn main() {
-    println!("Hello, world!");
+    // CLI definition
+    let matches = App::new("hub")
+        .version("0.1")
+        .author("James Gloudemans <james.gloudemans@gmail.com")
+        .about("Interact with Hubbub")
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .subcommand(
+            SubCommand::with_name("topic")
+                .about("Inspect and interact with Hubbub topics.")
+                .setting(AppSettings::SubcommandRequiredElseHelp)
+                .subcommand(
+                    SubCommand::with_name("list").about("List all the topics in the Hubbub graph."),
+                ),
+        )
+        .subcommand(
+            SubCommand::with_name("node")
+                .about("Inspect nodes in the Hubbub graph.")
+                .setting(AppSettings::SubcommandRequiredElseHelp)
+                .subcommand(
+                    SubCommand::with_name("list").about("List all the nodes in the Hubbub graph."),
+                ),
+        )
+        .get_matches();
+
+    // Arg / subcommand matching
+    if let Some(matches) = matches.subcommand_matches("topic") {
+        if let Some(matches) = matches.subcommand_matches("list") {
+            println!("topic list");
+        }
+    } else if let Some(matches) = matches.subcommand_matches("node") {
+        if let Some(matches) = matches.subcommand_matches("list") {
+            println!("node list");
+        }
+    }
 }
